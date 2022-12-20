4c468d55ead0b56364f0a52f585dea8d9f056451
lakdav <ladav2018@gmail.com>
2022-12-20 22:52:50 +0300

    fix code

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 const views = {
 	links: document.querySelectorAll('.navigation__list-link'),
 	currentLink: null,
+	header: document.getElementById('header'),
+	navigation: document.getElementById('navigation'),
 };
 
 window.addEventListener('hashchange', () => {
@@ -15,3 +17,19 @@ window.addEventListener('hashchange', () => {
 		}
 	});
 });
+let options = {
+	root: null,
+	rootMargin: '250px 0px 0px 0px',
+	threshold: 1,
+};
+let observer = new IntersectionObserver((entries, observer) => {
+	entries.forEach((entry) => {
+		console.log(entry);
+		if (!entry.isIntersecting) {
+			views.navigation.classList.add('stiky');
+		} else {
+			views.navigation.classList.remove('stiky');
+		}
+	});
+}, options);
+observer.observe(views.header);
